a6f32dc6285ce81fc9d2ede6d01706b9bdbc5688
pert <wil.paulk@gmail.com>
2016-12-29 00:54:58 -0500

    2nd commit
    Added functionality

--- move_func.py
+++ move_func.py
@@ -5,6 +5,7 @@ from tkinter.messagebox import showerror
 import tkinter as tk
 import shutil
 from datetime import datetime,timedelta
+import sqlite3
 
 
 
@@ -30,6 +31,72 @@ def ask_quit(self):
         self.master.destroy()
         os._exit(0)
 
+#=====================================================
+
+def create_db(self):
+    conn = sqlite3.connect('lastRun.db')
+    with conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE if not exists tbl_lastRun( \
+            ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+            col_source TEXT, \
+            col_dest TEXT, \
+            col_datetime TEXT \
+            );")
+        # commit to save changes & close the db connection
+        conn.commit()
+    conn.close()
+    first_run(self)
+
+def first_run(self):
+    data = ('DummyEntry', 'DummyEntry', datetime.today())
+    conn = sqlite3.connect('lastRun.db')
+    with conn:
+        cur = conn.cursor()
+        cur,count = count_records(cur)
+        if count < 1:
+            cur.execute("""INSERT INTO tbl_lastRun (col_source, col_dest, col_datetime) VALUES (?,?,?)""", data)
+            conn.commit()
+    conn.close()
+
+def onRefresh(self):
+    # Populate the listbox, coinciding with the database
+    self.txt_time.config(state='normal')
+    self.txt_time.delete(0.0,END)
+    conn = sqlite3.connect('lastRun.db')
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("""SELECT col_datetime FROM tbl_lastRun ORDER BY id DESC LIMIT 1;""")
+        varList = cursor.fetchone()
+        self.txt_time.insert(0.0,varList)
+
+    conn.close()
+    self.txt_time.config(state=DISABLED)
+
+def count_records(cur):
+    count = ""
+    cur.execute("""SELECT COUNT(*) FROM tbl_lastRun""")
+    count = cur.fetchone() [0]
+    return cur,count
+
+def addToList(self):
+    global source
+    global dest
+    var_firstField = str(self.txt_browse.get("1.0",'end-1c'))
+    var_secondField = str(self.txt_browse2.get("1.0",'end-1c'))
+    if(len(var_firstField)>0) and (len(var_secondField)>0):
+        conn = sqlite3.connect('lastRun.db')
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute("""INSERT INTO tbl_lastRun (col_source,col_dest,col_datetime) VALUES (?,?,?)""", [source, dest, datetime.today()])
+    else:
+        messagebox.showerror("Missing Text Error","Please ensure that there is data in all fields.")
+        
+
+
+#=====================================================
+
+    
 def load_file(self):
     fname = askdirectory()
     if fname:
@@ -53,18 +120,35 @@ def load_file2(self):
         return
 
 def move_files(self):
+    global source
+    global dest
     source = self.txt_browse.get("1.0",'end-1c')
     dest = self.txt_browse2.get("1.0",'end-1c')
+
+    conn = sqlite3.connect('lastRun.db')
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("""SELECT col_datetime FROM tbl_lastRun ORDER BY id DESC LIMIT 1;""")
+        count = cursor.fetchone()
+        last_backup = datetime.strptime(count[0], "%Y-%m-%d %H:%M:%S.%f")
+        current_time = datetime.strptime(str(datetime.today()), "%Y-%m-%d %H:%M:%S.%f")
+        difference = current_time - last_backup
 
     for file in glob.glob(os.path.join(source, '*.txt')):
         file_name = os.path.basename(file)
         time_info = os.path.getmtime(file)
         date = datetime.fromtimestamp(time_info)
-        if date > (datetime.today() - timedelta(1)):
+        print(date)
+        if (current_time - date) < (current_time - last_backup):
             shutil.move(file,dest)
             print(file_name + ' -Moved')
         else:
             print(file_name + ' -Not Moved')
+    addToList(self)
+    self.txt_time.config(state='normal')
+    self.txt_time.delete(0.0,END)
+    self.txt_time.insert(0.0,'{' + str(datetime.today()) + '}')
+    self.txt_time.config(state=DISABLED)
 
 if __name__ == "__main__":
     pass
--- move_gui.py
+++ move_gui.py
@@ -10,23 +10,31 @@ import move_func
 def load_gui(self):
 
     self.lbl_source = tk.Label(self.master,text='Source File:')
-    self.lbl_source.grid(row=0,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
+    self.lbl_source.grid(row=1,column=0,padx=(27,0),pady=(10,0),sticky=N+W)
     self.lbl_dest = tk.Label(self.master,text='Destination File:')
-    self.lbl_dest.grid(row=3,column=0,padx=(27,0),pady=(10,0),sticky =N+W)
+    self.lbl_dest.grid(row=4,column=0,padx=(27,0),pady=(10,0),sticky =N+W)
     self.lbl_move = tk.Label(self.master,text='Move Files:')
-    self.lbl_move.grid(row=5,column=0,padx=(27,0),pady=(10,0),sticky =N+W)
+    self.lbl_move.grid(row=6,column=0,padx=(27,0),pady=(10,0),sticky =N+W)
+    self.lbl_time = tk.Label(self.master,text='Last Updated:')
+    self.lbl_time.grid(row=0,column=0,padx=(27,0),pady=(10,0),sticky =N+W)
 
     self.txt_browse = Text(self.master, width = 40, height =1)
-    self.txt_browse.grid(row=1,column=2,pady=(5,0), sticky=W+E)
+    self.txt_browse.grid(row=2,column=1,pady=(5,0), sticky=W+E)
     self.txt_browse2 = Text(self.master, width = 40, height =1)
-    self.txt_browse2.grid(row=4,column=2,pady=(5,0), sticky=W+E)
+    self.txt_browse2.grid(row=5,column=1,pady=(5,0), sticky=W+E)
+    self.txt_time = Text(self.master, width = 5, height =1)
+    self.txt_time.grid(row=0,column=1,pady=(5,0), sticky=W+E+S)
+    self.txt_time.config(state=DISABLED)
 
     self.btn_browse = tk.Button(self.master,width=10,height=1,text='Browse',command=lambda: move_func.load_file(self))
-    self.btn_browse.grid(row=1, column=0, padx=(25,0),pady=(5,0), sticky=W)
+    self.btn_browse.grid(row=2, column=0, padx=(25,0),pady=(5,0), sticky=W)
     self.btn_browse2 = tk.Button(self.master,width=10,height=1,text='Browse',command=lambda: move_func.load_file2(self))
-    self.btn_browse2.grid(row=4, column=0, padx=(25,0),pady=(5,0), sticky=W)
-    self.btn_execute = tk.Button(self.master,width=40,height=6,text='Move Files',command=lambda: move_func.move_files(self))
-    self.btn_execute.grid(row=6,column=0, columnspan=3,padx=(40,0),pady=(15,0),sticky=N)
+    self.btn_browse2.grid(row=5, column=0, padx=(25,0),pady=(5,0), sticky=W)
+    self.btn_execute = tk.Button(self.master,width=40,height=5,text='Move Files',command=lambda: move_func.move_files(self))
+    self.btn_execute.grid(row=7,column=0, columnspan=3,padx=(80,0),pady=(0,0),sticky=N)
+
+    move_func.create_db(self)
+    move_func.onRefresh(self)
 
 
 if __name__=="__main__":
